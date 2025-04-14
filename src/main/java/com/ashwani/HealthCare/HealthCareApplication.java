e5fe1078ee9835e95b54472e47fdd8cae2ceba72
Vikas Sharma <vikaskolhua20@gmail.com>
2025-04-14 22:25:55 +0530

    update

--- src/main/java/com/ashwani/HealthCare/HealthCareApplication.java
+++ src/main/java/com/ashwani/HealthCare/HealthCareApplication.java
@@ -2,12 +2,22 @@ package com.ashwani.HealthCare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HealthCareApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure().ignoreIfMalformed().ignoreIfMissing().load();
+		System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
+		System.setProperty("API_KEY", dotenv.get("API_KEY"));
 		SpringApplication.run(HealthCareApplication.class, args);
 	}
 
 }
+
+
+
